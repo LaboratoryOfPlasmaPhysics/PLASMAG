b08817cdd70f546c9672ff5f47740b09b0c743a4
Claire Revillet <claire.revillet@cnrs-orleans.fr>
2024-09-13 16:36:30 +0200

    DOC - complete Nz strategy documentation

--- src/model/strategies/strategy_lib/Nz.py
+++ src/model/strategies/strategy_lib/Nz.py
@@ -1,22 +1,31 @@
 from src.model.input_parameters import InputParameters
 from src.model.strategies import CalculationStrategy
 
+
 class AnalyticalNzStrategy(CalculationStrategy):
-    """
-    TODO: Add description
+    """Magnetometric demagnetizing coefficient in the core axis direction for a cylindrical core shape
+
+    .. math::
+
+        N_z = \\frac{d_{core}}{2 . l_{core} + d_{core}}
+
+    with:
+        - :math:`d_{core}` : the diameter of the core
+        - :math:`l_{core}` : the length of the core
+
     """
 
     def calculate(self, dependencies: dict, parameters: InputParameters):
-        diam_core = parameters.data['diam_core']
-        len_core = parameters.data['len_core']
-        result = diam_core /(2 * (len_core) + diam_core)
+        diam_core = parameters.data["diam_core"]
+        len_core = parameters.data["len_core"]
+        result = diam_core / (2 * len_core + diam_core)
 
         return {
             "data": result,
-            "units" : [""],
-            "labels" : ["Nz"]
+            "units": [""],
+            "labels": ["Nz"]
         }
 
     @staticmethod
     def get_dependencies():
-        return ['diam_core', 'len_core']
+        return ["diam_core", "len_core"]
