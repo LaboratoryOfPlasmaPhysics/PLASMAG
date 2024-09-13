74984dab5348cd0cec3ce211cd9e4ebb685ab1bc
Claire Revillet <claire.revillet@cnrs-orleans.fr>
2024-09-13 16:58:34 +0200

    DOC - complete mu_app strategy documentation

--- src/model/strategies/strategy_lib/mu_app.py
+++ src/model/strategies/strategy_lib/mu_app.py
@@ -4,13 +4,20 @@ from src.model.strategies import CalculationStrategy
 
 
 class AnalyticalMu_appStrategy(CalculationStrategy):
-    """
-    TODO: Add description
+    """Analytical model of the effective permeability for a cylindrical core
+
+    .. math::
+        \\mu_{app} = \\frac{\mu_r}{1 + (N_z . (\\mu_r - 1))}
+
+    with:
+        - :math:`\\mu_r` : the relative permeability of the core material
+        - :math:`N_z` : the demagnetizing factor, related to the shape of the material
+
     """
 
     def calculate(self, dependencies: dict, parameters: InputParameters):
-        mu_r = parameters.data['mu_r']
-        Nz = dependencies['Nz']['data']
+        mu_r = parameters.data["mu_r"]
+        Nz = dependencies["Nz"]["data"]
         result = mu_r / (1 + (Nz * (mu_r - 1)))
 
         return {
@@ -21,4 +28,4 @@ class AnalyticalMu_appStrategy(CalculationStrategy):
 
     @staticmethod
     def get_dependencies():
-        return ['mu_r', 'Nz']
+        return ["mu_r", "Nz"]
