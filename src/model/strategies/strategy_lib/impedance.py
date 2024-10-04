bd3726f13022d0aebe5873f97b90fa8d5a1bd79b
Claire Revillet <claire.revillet@cnrs-orleans.fr>
2024-10-04 16:57:13 +0200

    DOC - complete impedance strategy documentation QUAL - clean imports and improve code readability

--- src/model/strategies/strategy_lib/impedance.py
+++ src/model/strategies/strategy_lib/impedance.py
@@ -1,34 +1,43 @@
-import numpy as np
+from numpy import pi, sqrt, column_stack
 from src.model.input_parameters import InputParameters
 from src.model.strategies import CalculationStrategy
 
 
 class AnalyticalImpedanceStrategy(CalculationStrategy):
+    """Analytical model of the impedance
+
+    .. math::
+
+        Z(f) = \sqrt{\\frac{R^2 + \omega^2}{(1 - L . C . \omega^2)^2 + (\omega . R . C)^2}}
+
+    with:
+        - :math:`R` : resistance of the coil
+        - :math:`L` : inductance of the coil
+        - :math:`C` : capacitance of the coil
+        - :math:`\omega = 2 \pi f`
+        - :math:`f` : frequency
+
+    """
 
     def calculate(self, dependencies: dict, parameters: InputParameters):
-        R = dependencies['resistance']['data']
-        L = dependencies['inductance']['data']
-        C = dependencies['capacitance']['data']
+        R = dependencies["resistance"]["data"]
+        L = dependencies["inductance"]["data"]
+        C = dependencies["capacitance"]["data"]
 
-        frequency_vector = dependencies['frequency_vector']['data']
+        frequency_vector = dependencies["frequency_vector"]["data"]
 
-
-        vectorized_impedance = np.vectorize(self.calculate_impedance)
-        impedance_values = vectorized_impedance(R, L, C, frequency_vector)
-        frequency_impedance_tensor = np.column_stack((frequency_vector, impedance_values))
-        result = frequency_impedance_tensor
+        impedance_num = (R ** 2) + (L * 2 * pi * frequency_vector) ** 2
+        impedance_den = (1 - L * C * (2 * pi * frequency_vector) ** 2) ** 2 \
+                        + (R * C * (2 * pi * frequency_vector)) ** 2
+        impedance_values = sqrt(impedance_num / impedance_den)
+        result = column_stack((frequency_vector, impedance_values))
 
         return {
             "data": result,
             "labels": ["Frequency", "Impedance"],
             "units": ["Hz", "Ohm"]
         }
-    def calculate_impedance(self, R, L, C, f):
-        impedance_num = (R ** 2) + (L * 2 * np.pi * f) ** 2
-        impedance_den = (1 - L * C * (2 * np.pi * f) ** 2) ** 2 + (R * C * (2 * np.pi * f)) ** 2
-        return np.sqrt(impedance_num / impedance_den)
 
     @staticmethod
     def get_dependencies():
-        return ['resistance', 'inductance', 'capacitance', 'frequency_vector']
-
+        return ["resistance", "inductance", "capacitance", "frequency_vector"]
