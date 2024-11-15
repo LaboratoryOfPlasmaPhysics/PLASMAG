fda692e559fc51affc65bcd0321f6a2506c24337
MaxRonce <max.ron@outlook.fr>
2024-11-15 16:38:47 +0100

    FIX - fix documentation compilation error

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,11 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
-from PLASMAG import version
-
-# Add path to the src directory
-sys.path.insert(0, os.path.abspath('../src'))
 
 project = 'PLASMAG'
 copyright = '2024, Maxime RONCERAY, Claire REVILLET'
 author = 'Maxime RONCERAY, Claire REVILLET'
-release = version
+release = "1.2.1"
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosectionlabel']
 autoclass_content = 'class'
